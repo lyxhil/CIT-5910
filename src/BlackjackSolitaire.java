bc095ab307d7a7440079108f12b6a3a99e2d364d
lyxhil <164365763+lyxhil@users.noreply.github.com>
2024-10-26 10:29:03 -0400

    BlackjackSolitaire.java play function

--- src/BlackjackSolitaire.java
+++ src/BlackjackSolitaire.java
@@ -1,5 +1,87 @@
-public class BlackjackSolitaire {
-    public static void play() {
+import java.util.Scanner;
 
+public class BlackjackSolitaire {
+    private Deck deck;
+    private String[][] grid;
+    private int discardsRemaining;
+    private int cardsInGrid;
+
+    public BlackjackSolitaire() {
+        deck = new Deck();
+
+        grid =  new String[4][];
+        grid[0] = {"1", "2", "3", "4", "5"};
+        grid [1] = {"6", "7", "8", "9", "10"};
+        grid [2] = {"11", "12", "13"};
+        grid [3] = {"14", "15", "16"};
+
+        discardsRemaining = 4;
+        cardsInGrid = 0;
     }
+
+    public void play() {
+        Scanner in = new Scanner(System.in);
+
+        while (cardsInGrid < 16) {
+            displayGrid();
+            Card top = deck.getTopCard();
+            deck.removeTopCard();
+            System.out.println("You drew: " + top.toString());
+            System.out.println("Are you placing this card in the grid (key in 1) or discarding this card (key in 2)?");
+            int placeInGrid = in.nextInt();
+            if (placeInGrid == 1) {
+                System.out.println("Please select the position (1 to 16) to place this card.");
+                selectGridPosition();
+            } else if (placeInGrid == 2 && discardsRemaining > 0) {
+                discardsRemaining--;
+            } else if (placeInGrid == 2 && discardsRemaining == 0) {
+                System.out.println("You have hit the maximum discards. Please select the position (1 to 16) to place this card");
+                selectGridPosition();
+            } else {
+                System.out.println("Invalid input: Please key in 1 to place card in grid or 2 to discard card");
+            }
+        }
+
+        int score = calculateScore();
+    }
+
+    public void displayGrid() {
+        System.out.println(grid);
+        System.out.println("Discards remaining: " + discardsRemaining);
+        }
+
+    public void selectGridPosition() {
+        boolean validPosition = false;
+
+        Scanner in = new Scanner(System.in);
+        String position = in.next();
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; i++) {
+                if (position,equals(grid[i][j])) {
+                    validPosition = true;
+                }
+            }
+        }
+
+        if (validPosition) {
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[i].length; i++) {
+                    if (position == grid[i][j]) {
+                        grid[i][j] = position;
+                        cardsInGrid++;
+                    }
+                }
+            }
+        } else {
+            System.out.println("Position " + position + "has been filled. Please select another unfilled position.");
+            selectGridPosition();
+        }
+    }
+
+    public int calculateScore() {
+        int score = 0;
+        return score;
+    }
+
 }
--- src/Deck.java
+++ src/Deck.java
@@ -2,33 +2,33 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Deck {
-    private ArrayList<Card> cards;
+    private ArrayList<Card> deck;
     private int currentIndex;
 
     public Deck() {
-        ArrayList<Card> cards = new ArrayList<Card>();
+        deck = new ArrayList<Card>();
         char[] suit = {'D', 'H', 'S', 'C'};
         String[] rank = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'};
 
         // Deck of 52 cards
         for (int i = 0; i < suit.length; i++) {
             for (int j = 0; j < rank.length; j++) {
-                cards.add(new Card(rank, suit));
+                deck.add(new Card(rank, suit));
             }
         }
 
-        Collections.shuffle(cards);
+        Collections.shuffle(deck);
         currentIndex = 0;
     }
 
     public Card getTopCard() {
-        if (!cards.isEmpty()) {
-            return cards.get(0);
+        if (!deck.isEmpty()) {
+            return deck.get(0);
         }
         return null;
     }
 
     public void removeTopCard() {
-        cards.remove(0);
+        deck.remove(0);
     }
 }
